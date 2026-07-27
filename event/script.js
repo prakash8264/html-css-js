6a833c7336a631dc45bae277ce47fd8aeb9dcbf9
Prakash Gupta <prgupta@enacton.email>
2026-07-27 11:54:38 +0530

    Add initial files for event and rock-paper-scissors games with basic functionality and styles

---
+++ event/script.js
@@ -0,0 +1,44 @@
+const myBox = document.getElementById("myBox");
+
+const moveAmount = 10;
+
+let x = 0;
+let y = 0;
+
+
+document.addEventListener("keydown", event =>{
+  myBox.textContent = "😮 WoW!";
+  myBox.style.background = "tomato"
+})
+
+document.addEventListener("keyup", event =>{
+  myBox.textContent = "😀";
+  myBox.style.background = "lightBlue"
+})
+
+document.addEventListener("keydown", event => {
+
+  if(event.key.startsWith("Arrow")){
+
+    event.preventDefault();
+
+    switch(event.key){
+      case "ArrowUp":
+        y -= moveAmount;
+        break;
+      case "ArrowDown":
+        y += moveAmount;
+        break;
+      case "ArrowLeft":
+        x -= moveAmount;
+        break;
+      case "ArrowRight":
+        x += moveAmount;
+        break;
+    }
+
+    myBox.style.top = `${y}px`;
+    myBox.style.left = `${x}px`;
+  }
+
+})
